04c43f34e5d04b8641715cab6370bf16da688cda
vikz-spec <vikz.shin@gmail.com>
2025-12-30 17:45:06 -0600

    absolute minimal server test

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
 const express = require("express");
-const path = require("path");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-// Serve frontend files
-app.use(express.static(path.join(__dirname, "public")));
-
-// Root route (IMPORTANT)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.send("IT WORKS");
 });
 
-// Basic socket connection (kept minimal for stability)
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
-
-// Railway requires THIS
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log("🚀 Server running on port", PORT);
+app.listen(PORT, () => {
+  console.log("SERVER LISTENING ON", PORT);
 });
