2b88a7222e0cf45eb78f7764f15f1e307cb14216
vikz-spec <vikz.shin@gmail.com>
2025-12-30 18:30:49 -0600

    Restore full game server

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("IT WORKS");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+// Socket.io
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-app.listen(PORT, () => {
-  console.log("SERVER LISTENING ON", PORT);
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("🚀 Server running on port", PORT);
 });
