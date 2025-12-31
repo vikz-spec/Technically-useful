b95cb1b96f5f36a6b42c304c283f562529248cc5
vikz-spec <vikz.shin@gmail.com>
2025-12-30 19:15:29 -0600

    Fix Not Found with catch-all route

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
 });
